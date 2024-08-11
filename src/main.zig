e0c71aa4fb3e91fc0f9ae3973137461a7f406684
Logan Wessel <lgwessel812@gmail.com>
2024-08-11 14:05:59 -0500

    add multithreading

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ fn timing(time: f128) void {
     std.debug.print("{d} h {d} m {d:.9} s\n", .{ h, m, s });
 }
 
-fn calc(nRolls: usize) usize {
+fn calc(nRolls: usize) void {
     var count: usize = 0;
     var max: usize = 0;
     var rolls: usize = 0;
@@ -34,26 +34,24 @@ fn calc(nRolls: usize) usize {
         // std.debug.print("Roll: {d}\n", .{rolls});
     }
 
-    return max;
+    std.debug.print("Max: {d}\n", .{max});
+    std.debug.print("Rolls: {d}\n", .{rolls});
 }
 
 pub fn main() !void {
-    const nThreads = 1;
-    const nRolls = 100_000;
+    const nThreads: usize = 16;
+    var nRolls: usize = 1_000_000_000;
 
     var count: usize = 0;
     var max: usize = 0;
     var rolls: usize = 0;
 
-    var start: i128 = 0;
-    var end: i128 = 0;
-
     switch (nThreads) {
         0 => std.debug.print("You need to use more than 0 threads.\n", .{}),
         1 => {
             std.debug.print("Running on 1 thread\n", .{});
-            // std.debug.print("Start timing\n", .{});
-            start = std.time.nanoTimestamp();
+
+            const start: i128 = std.time.nanoTimestamp();
 
             while (max < 177 and rolls < nRolls) {
                 count = 0;
@@ -72,18 +70,32 @@ pub fn main() !void {
                 // std.debug.print("Roll: {d}\n", .{rolls});
             }
 
-            end = std.time.nanoTimestamp();
+            const end: i128 = std.time.nanoTimestamp();
+
+            std.debug.print("Max: {d}\n", .{max});
+            std.debug.print("Rolls: {d}\n", .{rolls});
+
+            timing(@floatFromInt(end - start));
         },
         else => {
             std.debug.print("Running on {d} threads\n", .{nThreads});
-            start = std.time.nanoTimestamp();
-            max = calc(nRolls);
-            end = std.time.nanoTimestamp();
+            nRolls = @divTrunc(nRolls, nThreads);
+
+            var threads: [nThreads]std.Thread = undefined;
+
+            const start: i128 = std.time.nanoTimestamp();
+
+            for (0..nThreads) |i| {
+                threads[i] = try std.Thread.spawn(.{}, calc, .{nRolls});
+            }
+
+            for (threads) |thread| {
+                thread.join();
+            }
+
+            const end: i128 = std.time.nanoTimestamp();
+
+            timing(@floatFromInt(end - start));
         },
     }
-
-    std.debug.print("End timing\n", .{});
-    std.debug.print("Max: {d}\n", .{max});
-    std.debug.print("Rolls: {d}\n", .{rolls});
-    timing(@floatFromInt(end - start));
 }
