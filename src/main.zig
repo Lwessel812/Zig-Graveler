bbd5a02170126594a635e7de65ef214f4a0f45cb
Logan Wessel <lgwessel812@gmail.com>
2024-08-13 18:33:22 -0500

    fix logic

--- src/main.zig
+++ src/main.zig
@@ -12,18 +12,16 @@ fn timing(time: f128) void {
     std.debug.print("{d} h {d} m {d:.9} s\n", .{ h, m, s });
 }
 
-// fn calc(nRolls: usize) void {
-fn calc(nRolls: usize) void {
-    // For some reason passing the existing randomization from main() was way way way slow, like going from .5s -> 20 or 40
+fn calc(nRounds: usize) void {
     const seed: u64 = @intCast(std.time.microTimestamp());
     var rand = rng.init(seed);
     var randS = rand.random();
 
     var count: usize = 0;
-    var max: usize = 0;
-    var rolls: usize = 0;
+    var pTurn: usize = 0; // Paralysis turns
+    var rounds: usize = 0;
 
-    while (max < 177 and rolls < nRolls) {
+    while (pTurn < 177 and rounds < nRounds) {
         count = 0;
 
         // Roll 231 4 sided dice
@@ -35,54 +33,61 @@ fn calc(nRolls: usize) void {
             }
         }
 
-        if (count > max) {
-            max = count;
+        if (count > pTurn) {
+            pTurn = count;
         }
 
-        rolls += 1;
-        // std.debug.print("Roll: {d}\n", .{rolls});
+        rounds += 1;
     }
 
-    std.debug.print("Max: {d}\n", .{max});
-    std.debug.print("Rolls: {d}\n", .{rolls});
+    std.debug.print("pTurn: {d}\n", .{pTurn});
+    std.debug.print("rounds: {d}\n", .{rounds});
 }
 
-// fn smartCalc(nRolls: usize) void {
-fn smartCalc(nRolls: usize) void {
-    // For some reason passing the existing randomization from main() was way way way slow, like going from .5s -> 20 or 40
+fn smartCalc(nRounds: usize) void {
     const seed: u64 = @intCast(std.time.microTimestamp());
     var rand = rng.init(seed);
     var randS = rand.random();
 
-    var count: usize = 0;
-    var max: usize = 0;
-    var rolls: usize = 0;
+    var rounds: usize = 0;
+    var pTurn: usize = 0; // Paralysis turns
+    var sTurn: usize = 0; // Safe turns
+    var total: usize = 0; // Total turns
+    var best: usize = 0; // Best total turns made
 
-    while (max < 177 and rolls < nRolls) {
-        count = 0;
+    while (sTurn < 177 and rounds < nRounds) {
+        sTurn = 0;
+        pTurn = 0;
+        total = 0;
 
-        // Roll 231 4 sided dice
         for (0..231) |_| {
-            // We dont actually care which number its comparing to, we just need a 25% chance
-            // if (rand.uintLessThan(usize, 4) == 0) {
             if (randS.uintLessThan(usize, 4) == 0) {
-                count += 1;
+                pTurn += 1;
             } else {
-                // We can also just stop checking when we miss a check, this save so so so so much time its dumb
+                sTurn += 1;
+            }
+
+            total += 1;
+
+            if (sTurn > 54) {
                 break;
             }
         }
 
-        if (count > max) {
-            max = count;
+        if (total > best) {
+            best = total;
         }
 
-        rolls += 1;
-        // std.debug.print("Roll: {d}\n", .{rolls});
+        rounds += 1;
     }
 
-    std.debug.print("Max: {d}\n", .{max});
-    std.debug.print("Rolls: {d}\n", .{rolls});
+    if (best >= 231) {
+        std.debug.print("\n\nPASSED! We killed Rattata and Zubat!\n\n", .{});
+        return;
+    }
+
+    std.debug.print("best: {d} / 231 needed\n", .{best});
+    std.debug.print("rounds: {d}\n\n", .{rounds});
 }
 
 pub fn main() !void {
@@ -91,12 +96,17 @@ pub fn main() !void {
     var randS = rand.random();
 
     // const nThreads: usize = 1;
+    // var nRounds: usize = 1_000;
+
     const nThreads: usize = 16;
-    var nRolls: usize = 1_000_000_000;
+    var nRounds: usize = 1_000_000_000;
 
     var count: usize = 0;
-    var max: usize = 0;
-    var rolls: usize = 0;
+    var rounds: usize = 0;
+    var pTurn: usize = 0; // Paralysis turns
+    var sTurn: usize = 0; // Safe turns
+    var total: usize = 0; // Total turns
+    var best: usize = 0; // Best total turns made
 
     const smart: bool = true;
 
@@ -105,7 +115,7 @@ pub fn main() !void {
         1 => {
             if (smart) {
                 // I dont call calc() here because it ended up adding like 1% overhead to the time, and I dont really want the threads timing themselves anyway
-                std.debug.print("Running {d} rolls smartly on 1 thread\n", .{nRolls});
+                std.debug.print("Running {d} rounds smartly on 1 thread\n", .{nRounds});
 
                 const start: i128 = std.time.nanoTimestamp(); // Nanoseconds, because why not
                 // For some context in --release=fast on my laptop
@@ -113,38 +123,46 @@ pub fn main() !void {
                 // subtraction takes between 20-80 ns
                 // std.debug.print takes between 16k-21k ns
 
-                while (max < 177 and rolls < nRolls) {
-                    count = 0;
+                while (sTurn < 177 and rounds < nRounds) {
+                    sTurn = 0;
+                    pTurn = 0;
+                    total = 0;
 
-                    // Roll 231 4 sided dice
                     for (0..231) |_| {
-                        // We dont actually care which number its comparing to, we just need a 25% chance
-                        // if (rand.uintLessThan(usize, 4) == 0) {
                         if (randS.uintLessThan(usize, 4) == 0) {
-                            count += 1;
+                            pTurn += 1;
                         } else {
-                            // We can also just stop checking when we miss a check, this save so so so so much time its dumb
+                            sTurn += 1;
+                        }
+
+                        total += 1;
+
+                        if (sTurn > 54) {
                             break;
                         }
                     }
 
-                    if (count > max) {
-                        max = count;
+                    if (total > best) {
+                        best = total;
                     }
 
-                    rolls += 1;
-                    // std.debug.print("Roll: {d}\n", .{rolls});
+                    rounds += 1;
                 }
 
                 const end: i128 = std.time.nanoTimestamp();
 
-                std.debug.print("Max: {d}\n", .{max});
-                std.debug.print("Rolls: {d}\n", .{rolls});
+                if (best >= 231) {
+                    std.debug.print("\n\nPASSED! We killed Rattata and Zubat!\n\n", .{});
+                    return;
+                }
+
+                std.debug.print("best: {d} / 231 needed\n", .{best});
+                std.debug.print("rounds: {d}\n", .{rounds});
 
                 timing(@floatFromInt(end - start));
             } else {
                 // I dont call calc() here because it ended up adding like 1% overhead to the time, and I dont really want the threads timing themselves anyway
-                std.debug.print("Running {d} rolls on 1 thread\n", .{nRolls});
+                std.debug.print("Running {d} rounds on 1 thread\n", .{nRounds});
 
                 const start: i128 = std.time.nanoTimestamp(); // Nanoseconds, because why not
                 // For some context in --release=fast on my laptop
@@ -152,7 +170,7 @@ pub fn main() !void {
                 // subtraction takes between 20-80 ns
                 // std.debug.print takes between 16k-21k ns
 
-                while (max < 177 and rolls < nRolls) {
+                while (pTurn < 177 and rounds < nRounds) {
                     count = 0;
 
                     // Roll 231 4 sided dice
@@ -164,30 +182,30 @@ pub fn main() !void {
                         }
                     }
 
-                    if (count > max) {
-                        max = count;
+                    if (count > pTurn) {
+                        pTurn = count;
                     }
 
-                    rolls += 1;
-                    // std.debug.print("Roll: {d}\n", .{rolls});
+                    rounds += 1;
+                    // std.debug.print("Roll: {d}\n", .{rounds});
                 }
 
                 const end: i128 = std.time.nanoTimestamp();
 
-                std.debug.print("Max: {d}\n", .{max});
-                std.debug.print("Rolls: {d}\n", .{rolls});
+                std.debug.print("pTurn: {d}\n", .{pTurn});
+                std.debug.print("rounds: {d}\n", .{rounds});
 
                 timing(@floatFromInt(end - start));
             }
         },
         else => {
             if (smart) {
-                std.debug.print("Running {d} rolls smartly on {d} threads\n", .{ nRolls, nThreads });
+                std.debug.print("Running {d} rounds smartly on {d} threads\n\n", .{ nRounds, nThreads });
             } else {
-                std.debug.print("Running {d} rolls on {d} threads\n", .{ nRolls, nThreads });
+                std.debug.print("Running {d} rounds on {d} threads\n\n", .{ nRounds, nThreads });
             }
 
-            nRolls = @divTrunc(nRolls, nThreads); // Truncate just in case nRolls is small enough this results in a float
+            nRounds = @divTrunc(nRounds, nThreads); // Truncate just in case nRounds is small enough this results in a float
 
             var threads: [1_024]std.Thread = undefined; // If you have more than 1k threads why are you running this program?
 
@@ -195,11 +213,11 @@ pub fn main() !void {
 
             if (smart) {
                 for (0..nThreads) |i| {
-                    threads[i] = try std.Thread.spawn(.{}, smartCalc, .{nRolls}); // Loop to spawn threads
+                    threads[i] = try std.Thread.spawn(.{}, smartCalc, .{nRounds}); // Loop to spawn threads
                 }
             } else {
                 for (0..nThreads) |i| {
-                    threads[i] = try std.Thread.spawn(.{}, calc, .{nRolls}); // Loop to spawn threads
+                    threads[i] = try std.Thread.spawn(.{}, calc, .{nRounds}); // Loop to spawn threads
                 }
             }
 
